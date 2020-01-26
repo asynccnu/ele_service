e412bcfb14e73957822e61cb18124d5d66177004
Humbertzhang <504490160@qq.com>
2020-01-26 15:09:06 +0800

    Update database.py

--- service/database.py
+++ service/database.py
@@ -1,14 +1,11 @@
 import os
 from motor.motor_asyncio import AsyncIOMotorClient
 
-MONGODB_HOST = os.getenv('MONGOHOST') or 'localhost'
-MONGODB_PORT = int(os.getenv('MONGOPORT') or '27017')
-MONGODB_USERNAME = os.getenv('MONGODB_USERNAME') or "muxi"
-MONGODB_PASSWORD = os.getenv('MONGODB_PASSWORD') or "nopassword"
+MONGO_URI = os.environ.get("MONGO_URI") or "mongodb://username:secret@localhost:27017/?authSource=admin"
 
 async def setup_db():
     # client = AsyncIOMotorClient(MONGODB_HOST, MONGODB_PORT)
-    mongo_uri = "mongodb://{}:{}@{}:{}".format(MONGODB_USERNAME, MONGODB_PASSWORD, MONGODB_HOST, MONGODB_PORT)
+    mongo_uri = MONGO_URI
     client = AsyncIOMotorClient(mongo_uri)    
     dordb = client['dordb']         # 存储寝室电表号
     dorcol = dordb['dormitories']   # 寝室电表号集合
